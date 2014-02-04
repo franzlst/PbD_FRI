8892cfe117439b3c4af7015499aa571a6bcd0464
Franz Steinmetz <franz.steinmetz@aalto.fi>
2014-02-04 18:40:53 +0200

    Extended basic_moves: Current position and joint angles are printed when deploying setup_basic_moves.xml

--- basic_moves/src/basic_moves-component.cpp
+++ basic_moves/src/basic_moves-component.cpp
@@ -2,30 +2,69 @@
 #include <rtt/Component.hpp>
 #include <iostream>
 
+namespace iros {
+
+using namespace RTT;
+using namespace std;
+
 Basic_moves::Basic_moves(std::string const& name) : TaskContext(name){
-  std::cout << "Basic_moves constructed !" <<std::endl;
+	this->ports()->addEventPort( "JointsPort", joints_pos_port ).doc( "Retrieves joint positions and fires an event when receiving" );
+	this->ports()->addPort("PosePort", pose_port).doc("Retrieved Cartesian position and orientation.");
+	std::cout << "Basic_moves constructed !" <<std::endl;
 }
 
+Basic_moves::~Basic_moves()
+{}
+
 bool Basic_moves::configureHook(){
-  std::cout << "Basic_moves configured !" <<std::endl;
-  return true;
+	if(!joints_pos_port.connected()) {
+		std::cout << "Joints port not connected. Stopping..." << std::endl;
+		return false;
+	}
+
+	if(!pose_port.connected()) {
+		std::cout << "Pose port not connected. Stopping..." << std::endl;
+		return false;
+	}
+
+	std::cout << "Basic_moves configured !" <<std::endl;
+	return true;
 }
 
 bool Basic_moves::startHook(){
-  std::cout << "Basic_moves started !" <<std::endl;
-  return true;
+	std::cout << "Basic_moves started !" <<std::endl;
+	return true;
 }
 
 void Basic_moves::updateHook(){
-  std::cout << "Basic_moves executes updateHook !" <<std::endl;
+
+	if(joints_pos_port.read(current_joints_pos) == NewData)
+		cout << "New Cartesian Pos Data!" << endl;
+
+	//if(joints_pos_port.read(current_joints_pos) == NewData ||
+	if(pose_port.read(current_pose) == NewData) {
+		//std::cout << "Basic_moves received new data!" <<std::endl;
+		cout.precision(5);
+		cout << "Current Pos: " << current_pose.position.x << " - " << current_pose.position.y << " - "<< current_pose.position.z << endl;
+		cout << "And Joints: " << current_joints_pos.position.at(0) << " - "
+				<< current_joints_pos.position.at(1) << " - "
+				<< current_joints_pos.position.at(2) << " - "
+				<< current_joints_pos.position.at(3) << " - "
+				<< current_joints_pos.position.at(4) << " - "
+				<< current_joints_pos.position.at(5) << endl;
+	}
+
+	std::cout << "Basic_moves executes updateHook !" <<std::endl;
 }
 
 void Basic_moves::stopHook() {
-  std::cout << "Basic_moves executes stopping !" <<std::endl;
+	std::cout << "Basic_moves executes stopping !" <<std::endl;
 }
 
 void Basic_moves::cleanupHook() {
-  std::cout << "Basic_moves cleaning up !" <<std::endl;
+	std::cout << "Basic_moves cleaning up !" <<std::endl;
+}
+
 }
 
 /*
@@ -40,4 +79,4 @@ void Basic_moves::cleanupHook() {
  * If you have put your component class
  * in a namespace, don't forget to add it here too:
  */
-ORO_CREATE_COMPONENT(Basic_moves)
+ORO_CREATE_COMPONENT(iros::Basic_moves)
--- basic_moves/src/basic_moves-component.hpp
+++ basic_moves/src/basic_moves-component.hpp
@@ -2,14 +2,35 @@
 #define OROCOS_BASIC_MOVES_COMPONENT_HPP
 
 #include <rtt/RTT.hpp>
+#include <rtt/TaskContext.hpp>
+#include <rtt/Port.hpp>
+#include <geometry_msgs/typekit/Types.hpp>
+#include <sensor_msgs/typekit/Types.hpp>
+#include <kuka_lwr_fri/typekit/Types.hpp>
+#include <motion_control_msgs/typekit/Types.hpp>
+
+namespace iros {
+
+using namespace RTT;
 
 class Basic_moves : public RTT::TaskContext{
   public:
     Basic_moves(std::string const& name);
+    virtual ~Basic_moves();
+
     bool configureHook();
     bool startHook();
     void updateHook();
     void stopHook();
     void cleanupHook();
+
+  private:
+    InputPort< sensor_msgs::JointState >  joints_pos_port;
+    InputPort< geometry_msgs::Pose > pose_port;
+
+    sensor_msgs::JointState current_joints_pos;
+    geometry_msgs::Pose current_pose;
 };
+
+}
 #endif
